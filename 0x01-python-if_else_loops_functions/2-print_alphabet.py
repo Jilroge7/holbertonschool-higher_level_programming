7532a500da10aacff62f410ddf2c605f57f76fd2
Jilroge7 <1672@holbertonschool.com>
2020-04-28 23:19:19 +0000

    fourth variation is a charm for task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 # Printing ASCII alphabet in lowercase
-print("{}".format('abcdefghijklmnopqrstuvwxyz'), end="")
+for chars in range(97,123):
+    print("{:c}".format(chars), end='')
