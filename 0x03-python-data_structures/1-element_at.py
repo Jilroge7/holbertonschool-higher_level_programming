9d8e99c7768c523fee7e79476b4ae6b9b9f2ac88
Jilroge7 <1672@holbertonschool.com>
2020-05-05 23:37:38 +0000

    removing print debugging statements task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,11 +6,7 @@ def element_at(my_list, idx):
         return None
 
     for element in my_list:
-        print("{:d}: idx".format(idx))
-        print("{:d}: index".format(index))
-        print("{:d}:element".format(element))
         if index == idx:
-            print("idx equals element")
             return (element)
         index += 1
     if idx > index:
