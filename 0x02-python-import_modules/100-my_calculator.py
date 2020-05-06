bcab1e9ca3dac086bf35af3d5c4eef4bc7a70058
Jilroge7 <1672@holbertonschool.com>
2020-05-06 22:37:37 +0000

    again attempt to cover reqs for avanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
+    import calculator_1
     str1 = "Usage: ./100-my_calculator.py <a> <operator> <b>"
     str2 = "Unknown operator. Available operators: +, -, * and /"
     num = (len(sys.argv))
