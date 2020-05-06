ca7ee29e18b389b1fa2b405a17d2dcf4e6392b19
Jilroge7 <1672@holbertonschool.com>
2020-05-06 01:55:36 +0000

    edit to include if condition task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    for index in my_list:
-        print("{:d}".format(my_list[-1]))
-        my_list[-1] -= 1
+    if my_list:
+        for index in my_list:
+            print("{:d}".format(my_list[-1]))
+            my_list[-1] -= 1
