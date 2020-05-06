2b9b025234dde48ae359af8286e98275649f104f
Jilroge7 <1672@holbertonschool.com>
2020-05-06 00:33:51 +0000

    added wrong task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,13 +2,18 @@
 def replace_in_list(my_list, idx, element):
 
     index = 0
+
+    if (len(my_list) == 0):
+        return my_list
+
     if idx < 0:
-        return (my_list)
+        return my_list
 
     for elem in my_list:
         if index == idx:
             my_list[index] = element
-            return (my_list)
+            return my_list
         index += 1
+
     if idx > index:
-        return (my_list)
+        return my_list
