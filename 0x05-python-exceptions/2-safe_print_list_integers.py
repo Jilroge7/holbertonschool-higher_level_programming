8f38c248cb25cf587ca789eef2412f2cfa1fbfc4
Jilroge7 <1672@holbertonschool.com>
2020-05-19 00:54:55 +0000

    adding task 2 partial

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    index = 0
+
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            index += 1
+        except ValueError:
+            index += 1
+            pass
+        except TypeError:
+            index -= 1
+            break
+    print()
+    return (index)
