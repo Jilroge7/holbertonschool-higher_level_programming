39bc3b844455e1caf541df35705201c541228132
Jilroge7 <1672@holbertonschool.com>
2020-05-20 04:57:23 +0000

    adding task 2 partial

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pyton3
+"""Module here"""
+
+
+class Square:
+    """Square defined class with private attribute size"""
+    def __init__(self, size=0):
+        self.__size = size  # size is user input or prog input
+        if size is not 0:
+            self._size = size
+
+        if (str(size).isdigit()):
+            """protected instance attribute, check for integer"""
+            try:
+                if size >= 0:
+                    self._size = size
+            except ValueError:
+                print("size must be >= 0")
+            except:
+                print("size must be an integer")
