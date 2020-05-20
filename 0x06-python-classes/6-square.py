44bf252399c9aa1d7fc8cdca5bbdc2c2f166db75
Jilroge7 <1672@holbertonschool.com>
2020-05-20 22:35:07 +0000

    adding partialtask 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module for another elaborated class Square"""
+
+
+class Square:
+    """class Square with private attributes size and position"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """module to get value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Module to set value of size"""
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        elif not isinstance(size, int):
+                raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        """Module to get values for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Module to set values for position"""
+        if isinstance(position, tuple) and position > 0:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Public instance method to determine area"""
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        """Public instance method to print pattern of area square"""
+        if self.__size == 0:
+            print()
+        else:
+            if self.__position:
+                for i in tuple(self.__position):
+                    print("_")
+            else:
+                print()
+            for i in range(int(self.__size)):
+                for i in range(int(self.__size)):
+                    print("#", end="")
+                print()
