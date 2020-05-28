5d44b0b9bd387c5cfb53146ded2fdd317281e024
Jilroge7 <1672@holbertonschool.com>
2020-05-28 23:17:27 +0000

    adding partial advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for lockedclass"""
+
+
+class LockedClass:
+    """class LockedClass"""
+    def __init__(self):
+        self.lock = threading.Lock()
+        Lock.acquire(True)
