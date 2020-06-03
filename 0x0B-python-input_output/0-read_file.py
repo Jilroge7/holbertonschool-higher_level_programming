9620d5c2abaaa2b119e17068b4047c62f08924f1
Jilroge7 <1672@holbertonschool.com>
2020-06-03 22:18:17 +0000

    edit to task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Function to read a file to standard output"""
     with open('my_file_0.txt', encoding='utf-8') as a_file:
-        for line in a_file:
-            print(line, end="")
+        print(a_file.read(), end="")
