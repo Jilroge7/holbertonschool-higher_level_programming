6de138ed41e968c287891e62542ce8976ce4e525
Jilroge7 <1672@holbertonschool.com>
2020-06-03 02:38:02 +0000

    adding task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry():
+    """class BaseGeometry with 2 methods"""
+    pass
+
+    def area(self):
+        """area is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """pub Inst method to validate value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if isinstance(value, int):
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
