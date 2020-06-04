d04d3d25e359b11f80a63b2cc5e7bbcef2216ad5
Jilroge7 <1672@holbertonschool.com>
2020-06-04 03:08:50 +0000

    adding task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for task 9 script"""
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+my_list = []
+try:
+    load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+my_list = my_list + sys.argv[1:]
+save_to_json_file(my_list, "add_item.json")
