4340d18e465dc2fc1956877e9e76d92d57e2bc7a
Jilroge7 <1672@holbertonschool.com>
2020-06-04 19:03:10 +0000

    adding task 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -6,12 +6,17 @@ class Student():
     """class Student defines student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.last_name - last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Pub Method to retrieve dict repr of instance"""
-        if all(isinstance(value, str) for value in attrs):
-            return self.__dict__
+        if type(attrs) == list:
+            if all(isinstance(value, str) for value in attrs):
+                new_dict = {}
+                for value in attrs:
+                    if value in list(self.__dict__.keys()):
+                        new_dict[value] = self.__dict__[value]
+            return new_dict
         else:
             return self.__dict__
