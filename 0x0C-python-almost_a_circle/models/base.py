33585dca65733c95e7720375e5807ea9268464f8
Jilroge7 <1672@holbertonschool.com>
2020-06-09 02:31:37 +0000

    adding partial 16 and task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class 1.0"""
+import json
 
 
 class Base():
@@ -12,3 +13,29 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method to convert to json str repr"""
+        if list_dictionaries is not None:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return list repr of json str"""
+        if json_string is not None:
+            return json.loads(json_string)
+        else:
+            return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """cls method to save json str to file"""
+        with open('{}.json.'.format(cls), mode='w', encoding='UTF8') as a_file:
+            if list_objs:
+                json_list = Base.to_json_string([list_objs])
+            else:
+                json_list = []
+            a_file.write(json.dumps(json_list))
