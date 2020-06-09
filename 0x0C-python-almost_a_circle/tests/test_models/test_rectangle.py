3fce9ae1f233188ac5e3683b56de0b3afedc6401
Jilroge7 <1672@holbertonschool.com>
2020-06-09 22:26:19 +0000

    adding test_rect and pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,4 +2,32 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class CreateNewInstanceOfRectangleTestSuite(unittest.TestCase):
+class RectangleTestSuite(unittest.TestCase):
+    """Testing Rectangle subclass"""
+    def setUp(self):
+        """Initializing objects for test"""
+        self.r0 = Rectangle(10, 2)
+        self.r1 = Rectangle(2, 10)
+        self.r2 = Rectangle(10, 2, 0, 0, 12)
+        self.r3 = Rectangle(10, 2, 0, 0, -1)
+
+    def test_a_init(self):
+        """Test to return self.id from Rectangle"""
+        self.assertEqual(self.r0, 1)
+        self.assertEqual(self.r1, 2)
+        self.assertEqual(self.r2, 12)
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(3)
+        self.assertEqual(self.r3, -1)
+
+    def tearDown(self):
+        """Cleaning up after myself"""
+        del self.r0
+        del self.r1
+        del self.r2
+        del self.r3
+
+if __name__ == "__main__":
+    unittest.main()
