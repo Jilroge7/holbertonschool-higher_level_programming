a51d44fb79554cb631a904468020cad3967c7624
Jilroge7 <1672@holbertonschool.com>
2020-06-15 03:27:58 +0000

    fix to typo of y exception message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,7 +68,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
 
