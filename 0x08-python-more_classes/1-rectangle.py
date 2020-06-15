989dd06148460144427cc1ad1f8a4994711f80d3
Jilroge7 <1672@holbertonschool.com>
2020-06-15 03:50:25 +0000

    edit to validator

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,12 +16,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """method to set the value of width"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        elif isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
                 self.__width = value
 
     @property
@@ -32,10 +31,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """method to set the value of height"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        elif isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -16,13 +16,12 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """method to set the value of width"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        elif isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -32,13 +31,12 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """method to set the value of height"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        elif isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
     def area(self):
         """publ ins method to determine area of rect"""
@@ -54,9 +52,9 @@ class Rectangle:
     def __str__(self):
         """built in to return printed representation of string instance"""
         picture = ""
-        for i in range(int(self.__height)):
-            for i in range(int(self.__width)):
-                if int(self.__height) == 0 or int(self.__width) == 0:
+        for i in range(self.height):
+            for i in range(self.width):
+                if self.height == 0 or self.width == 0:
                     return picture
                 picture += "#"
             picture += '\n'
