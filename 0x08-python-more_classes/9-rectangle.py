deef063a2fe2c430a4188ec9851d829221137149
Jilroge7 <1672@holbertonschool.com>
2020-06-15 03:56:31 +0000

    edit to task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -20,13 +20,12 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Method to set value of width"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -36,13 +35,12 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Method to set value of height"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
     def area(self):
         """Pub Inst Method to determine area of rect"""
