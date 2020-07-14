02f0f0d0affaa3a22fa51ea1c2b057dbfba6dea2
Jilroge7 <1672@holbertonschool.com>
2020-07-14 22:48:41 +0000

    edit to task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- create a database and table
 -- add id, state_id, and name to table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(id) REFERENCES `states`(`id`), name VARCHAR (256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id INT NOT NULL, name VARCHAR (256) NOT NULL, FOREIGN KEY(state_id) REFERENCES state(id));
