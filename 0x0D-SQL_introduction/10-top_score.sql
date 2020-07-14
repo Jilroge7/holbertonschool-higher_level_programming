246114e234c4db1e3f313351384a4ccc36a1e089
Jilroge7 <1672@holbertonschool.com>
2020-07-14 03:17:37 +0000

    adding task 10 maybe right

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records in second_table
 -- List ordered by hishest score, show only name and score.
-SELECT name, score FROM second_list ORDER BY score DESC;
+SELECT score, name FROM `second_list` ORDER BY score DESC;
