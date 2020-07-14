a981f51ef42ac7ce5f46c20207e95f399e65ec47
Jilroge7 <1672@holbertonschool.com>
2020-07-14 22:01:09 +0000

    adding task line

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- create database and table
 -- add id and name to table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.states (id INT IDENTITY PRIMARY KEY, name VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_2.states (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
