0a91d62fdc9dc0e77a69c6bc183b3363aa9300e7
Jilroge7 <1672@holbertonschool.com>
2020-08-07 20:30:55 +0000

    adding task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module to list all states from a database """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    curse = conn.cursor()
+    curse.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    looky_rows = curse.fetchall()
+    for row in looky_rows:
+        print(row)
+    curse.close()
+    conn.close()
