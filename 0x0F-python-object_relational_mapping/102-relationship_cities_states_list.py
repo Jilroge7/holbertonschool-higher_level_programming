04c4a4b12c140da2735e7f424ce07aac8f435f4f
Jilroge7 <1672@holbertonschool.com>
2020-08-15 20:02:54 +0000

    adding adv 102

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script to list all city objects related to state frm database """
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import State, Base
+    from relationship_city import City
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    cities = session.query(City).all():
+        for city in cities:
+            print(" {}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
