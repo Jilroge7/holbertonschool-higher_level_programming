68604faa94ca4a523702f49c4b64474d61b8f917
Jilroge7 <1672@holbertonschool.com>
2020-08-15 19:11:00 +0000

    edit to add binary sytnax

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     curse = conn.cursor()
-    curse.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+    curse.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id ASC"
                   .format(sys.argv[4]))
     looky_rows = curse.fetchall()
     for row in looky_rows:
