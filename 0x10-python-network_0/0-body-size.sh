e7f488fa5a508eacabb52a791759cc403b765737
Jilroge7 <1672@holbertonschool.com>
2020-08-18 19:29:11 +0000

    adding task 0

--- 0x10-python-network_0/0-body-size.sh
+++ 0x10-python-network_0/0-body-size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # Script to takes a url, send request to it, and get body size in return.
-URL = $1
-
-curl "$URL" | grep Content-Length
+curl -sI "$1" | grep Content-Length | cut --complement -d ' ' -f1
