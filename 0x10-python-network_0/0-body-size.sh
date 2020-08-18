5da106ea47d7dad4f251446c52960044bc82d823
Jilroge7 <1672@holbertonschool.com>
2020-08-18 19:02:02 +0000

    adding task 0 and readme for pull into container

--- 0x10-python-network_0/0-body-size.sh
+++ 0x10-python-network_0/0-body-size.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Script to takes a url, send request to it, and get body size in return.
-URL=$1
+URL = $1
 
-curl $URL | grep Content-Length
+curl "$URL" | grep Content-Length
