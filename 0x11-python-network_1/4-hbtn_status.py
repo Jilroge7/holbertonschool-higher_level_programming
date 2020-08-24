4caa91e68b4fe1d6b6b9dd06836a0cf16d9399b1
Jilroge7 <1672@holbertonschool.com>
2020-08-24 23:10:15 +0000

    edit to use requests in task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ script to fetch url """
 if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        https = response.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(https)))
-        print("\t- content: {}".format(https))
+    import requests
+    response = requests.get('https://intranet.hbtn.io/status')
+    response.encoding = 'utf-8'
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
