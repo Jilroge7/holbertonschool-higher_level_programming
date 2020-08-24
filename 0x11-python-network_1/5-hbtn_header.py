5e1a706a4ecfc5e6fd97deeab31adc381117f0d0
Jilroge7 <1672@holbertonschool.com>
2020-08-24 23:35:48 +0000

    edit to add get to response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import requests
     import sys
     response = requests.get(sys.argv[1])
-    header = response.headers['X-Request-Id']
+    header = response.headers.get('X-Request-Id')
     print(header)
