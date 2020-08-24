cd788c450b877dc4e91cf2eba4a452bcbd31e21e
Jilroge7 <1672@holbertonschool.com>
2020-08-24 21:42:48 +0000

    adding task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,11 @@
 """ script to send post req to url with email as param """
 if __name__ == "__main__":
     import sys
-    import urllib
+    from urllib import request
+    from urllib import parse
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    with request.Request(sys.argv[1], data) as reques:
-        with urllip.request.urlopen(reques) as response:
-            print(response.read())
+    data = parse.urlencode(values)
+    data = data.encode('utf-8')
+    with request.urlopen(sys.argv[1], data) as response:
+            data = response.read().decode('utf-8')
+            print(data)
