9edd143942fc8b661379b17731b4c3ea5d58a4f1
Jilroge7 <1672@holbertonschool.com>
2020-09-15 19:03:38 +0000

    fix to capitalization and shorten

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-  const statement = 'No Argument';
-  console.log(statement);
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-  const statement = 'Argument found';
-  console.log(statement);
+  console.log('Argument found');
 } else {
-  const statement = 'Arguments found';
-  console.log(statement);
+  console.log('Arguments found');
 }
