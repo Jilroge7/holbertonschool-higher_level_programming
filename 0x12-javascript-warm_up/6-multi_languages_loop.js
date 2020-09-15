8743e371a8b87f89cd93eade51e2fcff417a8d47
Jilroge7 <1672@holbertonschool.com>
2020-09-15 20:41:59 +0000

    adding task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let iterate;
+for (iterate = 0; iterate < myVar.length; iterate++) {
+  console.log(myVar[iterate]);
+}
