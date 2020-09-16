c43a9e7d70b5ab353ea4d7d1b004da0115363246
Jilroge7 <1672@holbertonschool.com>
2020-09-16 01:15:25 +0000

    adding task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function add (a, b) {
+  const first = parseInt(process.argv[2]);
+  const second = parseInt(process.argv[3]);
+  if (second) {
+    console.log(first + second);
+  } else {
+    console.log('NaN');
+  }
+}
+add(process.argv[2], process.argv[3]);
