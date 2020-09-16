e0155388ec8699830e7755d909a46e1c810af7dc
Jilroge7 <1672@holbertonschool.com>
2020-09-16 18:36:48 +0000

    adding task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-add();
-function add (a, b) {
-  const first = parseInt(process.argv[2]);
-  const second = parseInt(process.argv[3]);
-  if (second) {
-    console.log(first + second);
-  } else {
-    console.log('NaN');
-  }
-}
-add(process.argv[2], process.argv[3]);
+exports.add = function (a, b) {
+  return a + b;
+};
