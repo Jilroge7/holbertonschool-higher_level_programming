f97c4c3dfd29aaba613cbe7df90934dec4b50e44
Jilroge7 <1672@holbertonschool.com>
2020-09-22 01:21:01 +0000

    adding task 8 partial

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let i;
+  const revList = [];
+  for (i = 0; i < list.length; i++) {
+
+  }
+  for (; i >= 0; i--) {
+    revList.push(list[i]);
+  }
+  console.log(revList);
+};
