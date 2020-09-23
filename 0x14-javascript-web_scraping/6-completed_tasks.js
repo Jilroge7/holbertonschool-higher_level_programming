2d08fecb9a629f9fdadb5f107a320cdeff02db23
Jilroge7 <1672@holbertonschool.com>
2020-09-23 19:41:43 +0000

    adding task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,19 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, result, body) {
-  console.log(JSON.parse(body));
-  if (error) {
+  if (error) return;
+  const doList = JSON.parse(body);
+  const dct = {};
+  for (let ct = 0; ct < doList.length; ct++) {
+    if (dct[doList[ct].userId] === undefined) {
+      if (doList[ct].completed === true) {
+        dct[doList[ct].userId] = 1;
+      }
+    } else {
+      if (doList[ct].completed === true) {
+        dct[doList[ct].userId] += 1;
+      }
+    }
   }
+  console.log(dct);
 });
